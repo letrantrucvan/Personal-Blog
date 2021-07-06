26c1da7a096e5676fdb927560d521cb75ecb42d2
van.le <van.le@nexpando.com>
2021-07-06 16:14:05 +0700

    Create TABLE team & stadium
    Create table with relations

--- blog-strapi/config/server.js
+++ blog-strapi/config/server.js
@@ -1,9 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b282aec340c396d53291a7f2cb6f9563'),
+      secret: env('ADMIN_JWT_SECRET','b282aec340c396d53291a7f2cb6f9563'),
     },
   },
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  cron: {
+    enabled: true
+  }
 });
+
